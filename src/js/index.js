4d548c09477dcec5fb93233de58bfd6756aee020
Кирилл <samsuu@ya.ru>
2020-07-02 19:54:49 +0700

    add second screen & complete first screen

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,13 @@
     const navMain = document.querySelector('.main-nav');
     const navToggle = document.querySelector('.main-nav__toggle');
     const popupCall = document.querySelector('.popup-call');
-    const navPhone = document.querySelector('.main-nav__phone');
+    const navPhone = document.querySelector('.main-nav__phone-icon-button');
+    const navPhoneText = document.querySelector('.main-nav__phone-text-button');
     const closePopupCall = document.querySelector('.popup-call__closed');
     const popupAddition = document.querySelector('.popup-addition');
     const navAddition = document.querySelector('.main-menu__item-link--addition');
     const closePopupAddition = document.querySelector('.popup-addition__closed');
+    const themeToggle = document.querySelector('.night-mode__toggle');
   
     navMain.classList.remove('main-nav--nojs');
   
@@ -21,6 +23,13 @@
       }
     });
     navPhone.addEventListener('click', () => {
+      console.log('click');
+      if (popupCall.classList.contains('popup-call--closed')) {
+        popupCall.classList.remove('popup-call--closed');
+        popupCall.classList.add('popup-call--opened');
+      }
+    });
+    navPhoneText.addEventListener('click', () => {
       if (popupCall.classList.contains('popup-call--closed')) {
         popupCall.classList.remove('popup-call--closed');
         popupCall.classList.add('popup-call--opened');
@@ -44,16 +53,26 @@
         popupAddition.classList.add('popup-addition--closed');
       }
     });
+    themeToggle.addEventListener('click', () => {
+      if (themeToggle.classList.contains('night-mode__toggle--off')) {
+        themeToggle.classList.remove('night-mode__toggle--off');
+        themeToggle.classList.add('night-mode__toggle--on');
+      } else {
+        themeToggle.classList.remove('night-mode__toggle--on');
+        themeToggle.classList.add('night-mode__toggle--off');
+      }
+    });
 
-    const changeMainLogo = () => { //TODO: change 
-      const logoFirst = document.querySelector('.logo__first');
+    //TODO: change 
+    const changeMainLogo = () => { 
+      /* const logoFirst = document.querySelector('.logo__first');
       const logoSecond = document.querySelector('.logo__second');
       logoFirst.textContent = 'L';
-      logoSecond.textContent = 'C';
+      logoSecond.textContent = 'C'; */
     }
-
     window.addEventListener('scroll', () => {
       changeMainLogo();
     });
+    // ----
   });
 })(jQuery)
